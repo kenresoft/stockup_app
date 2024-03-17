bc091b2e3d2912a0c9a098cddc67097180102438
kenresoft <amadikenneth8@gmail.com>
2024-03-17 21:38:19 +0100

    Update portfolio_clip_2.dart

--- lib/shape/portfolio_clip_2.dart
+++ lib/shape/portfolio_clip_2.dart
@@ -1,26 +1,92 @@
 import 'package:flutter/material.dart';
 
 class PortfolioClip2 extends CustomPainter {
+  const PortfolioClip2({required this.index});
+
+  final int index;
   @override
   void paint(Canvas canvas, Size size) {
-    final width = size.width - 10;
-    final height = size.height - 10;
+    final width = size.width;
+    final height = size.height;
 
     final paint = Paint()
-      //..color = const Color(0xff2B2B2B)
-      ..color = const Color(0xff83CCFB)
-      ..strokeWidth = 5
-      ..style = PaintingStyle.fill;
+      ..color = const Color(0xff414141)
+      //..color = const Color(0xff83CCFB)
+      ..strokeWidth = 3
+      ..style = PaintingStyle.stroke;
 
     final path = Path()
-      ..moveTo(0, 20)
-      /// Top Left
-      ..addRRect(RRect.fromLTRBR(0, 0, width, height, const Radius.circular(20)));
+
+      /// left box
+      ..moveTo(20, 0)
+      ..lineTo((width / 2) - 30, 0)
+
+      /// top curve
+      ..arcToPoint(Offset((width / 2) - 10, 10), radius: const Radius.circular(25))
+      ..quadraticBezierTo(
+        (width / 2),
+        (height / 2) + 0,
+        (width / 2) + 10,
+        10,
+      )
+      ..arcToPoint(Offset((width / 2) + 30, 0), radius: const Radius.circular(25))
+
+      /// right box
+      ..lineTo(width - 20, 0)
+      ..arcToPoint(Offset(width - 20, height), radius: const Radius.circular(25))
+
+      /// right box
+      ..lineTo((width / 2) + 30, height)
+
+      /// bottom curve
+      ..arcToPoint(Offset((width / 2) + 10, height - 10), radius: const Radius.circular(25))
+      ..quadraticBezierTo(
+        (width / 2),
+        (height / 2) - 0,
+        (width / 2) - 10,
+        height - 10,
+      )
+      ..arcToPoint(Offset((width / 2) - 30, height), radius: const Radius.circular(25))
+
+      /// left box
+
+      ..lineTo(20, height)
+      ..arcToPoint(const Offset(20, 0), radius: const Radius.circular(25));
+
+    /// draw fill
     canvas.drawPath(path, paint);
+
+    /// draw outline
+    canvas.drawPath(
+      path,
+      paint
+        ..style = PaintingStyle.fill
+        ..color = const Color(0xff333333),
+    );
+
+    index == 0
+        ? canvas.drawPath(
+            Path()
+              ..moveTo(20, 0)
+              ..lineTo((width / 2) - 30, 0)
+              ..arcToPoint(Offset((width / 2) - 30, height), radius: const Radius.circular(25))
+              ..lineTo(20, height)
+              ..arcToPoint(const Offset(20, 0), radius: const Radius.circular(25)),
+            paint..color = Colors.blue,
+          )
+        : canvas.drawPath(
+            Path()
+              ..moveTo((width / 2) + 30, 0)
+              ..lineTo(width - 20, 0)
+              ..arcToPoint(Offset(width - 20, height), radius: const Radius.circular(25))
+              ..lineTo((width / 2) + 30, height)
+              ..arcToPoint(Offset((width / 2) + 30, 0), radius: const Radius.circular(25)),
+            paint..color = Colors.lightBlueAccent,
+          );
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
