f541c8ddb28aaff2f457920c6dcd816ed8aeba48
kenresoft <amadikenneth8@gmail.com>
2024-03-31 22:47:35 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fontresoft/fontresoft.dart';
 import 'package:stockup_app/screens/funds.dart';
 import 'package:stockup_app/screens/home.dart';
@@ -16,26 +17,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'StockUp Mobile',
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
-        colorSchemeSeed: const Color(0xff84CEFE),
-        fontFamily: FontResoft.lato,
-        package: FontResoft.package,
-        highlightColor: Colors.transparent,
-        splashColor: Colors.transparent,
+    return ScreenUtilInit(
+      //designSize: const Size(360, 640),
+      //designSize: const Size(360, 820),
+      designSize: const Size(448, 998),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'StockUp Mobile',
+        theme: ThemeData(
+          useMaterial3: true,
+          brightness: Brightness.light,
+          colorSchemeSeed: const Color(0xff84CEFE),
+          fontFamily: FontResoft.lato,
+          package: FontResoft.package,
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+        ),
+        routes: {
+          '/': (context) => const Home(),
+          '/funds': (context) => const Funds(),
+          '/portfolio': (context) => const Portfolio(),
+          '/mpin': (context) => const MPin(),
+          '/withdraw': (context) => const Withdraw(),
+          '/invite': (context) => const Invite(),
+        },
       ),
-      routes: {
-        '/': (context) => const Home(),
-        '/funds': (context) => const Funds(),
-        '/portfolio': (context) => const Portfolio(),
-        '/mpin': (context) => const MPin(),
-        '/withdraw': (context) => const Withdraw(),
-        '/invite': (context) => const Invite(),
-      },
     );
   }
 }
