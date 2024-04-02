1d63b323857135f2afe5e8b74e3d6c376a3f4abe
kenresoft <amadikenneth8@gmail.com>
2024-04-02 16:30:06 +0100

    Update portfolio_clip_2.dart

--- lib/shapes/portfolio_clip_2.dart
+++ lib/shapes/portfolio_clip_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PortfolioSwitchClip extends CustomPainter {
   const PortfolioSwitchClip({required this.currentIndex});
@@ -17,40 +18,40 @@ class PortfolioSwitchClip extends CustomPainter {
     final path = Path()
 
       /// left box
-      ..moveTo(20, 0)
-      ..lineTo((width / 2) - 30, 0)
+      ..moveTo(20.w, 0)
+      ..lineTo((width / 2) - 30.w, 0)
 
       /// top curve
-      ..arcToPoint(Offset((width / 2) - 10, 10), radius: const Radius.circular(25))
+      ..arcToPoint(Offset((width / 2) - 10.w, 10.h), radius: const Radius.circular(25).r)
       ..quadraticBezierTo(
         (width / 2),
         (height / 2) + 0,
-        (width / 2) + 10,
-        10,
+        (width / 2) + 10.w,
+        10.h,
       )
-      ..arcToPoint(Offset((width / 2) + 30, 0), radius: const Radius.circular(25))
+      ..arcToPoint(Offset((width / 2) + 30.w, 0), radius: const Radius.circular(25).r)
 
       /// right box
-      ..lineTo(width - 20, 0)
-      ..arcToPoint(Offset(width - 20, height), radius: const Radius.circular(25))
+      ..lineTo(width - 20.w, 0)
+      ..arcToPoint(Offset(width - 20.w, height), radius: const Radius.circular(25).r)
 
       /// right box
-      ..lineTo((width / 2) + 30, height)
+      ..lineTo((width / 2) + 30.w, height)
 
       /// bottom curve
-      ..arcToPoint(Offset((width / 2) + 10, height - 10), radius: const Radius.circular(25))
+      ..arcToPoint(Offset((width / 2) + 10.w, height - 10.h), radius: const Radius.circular(25).r)
       ..quadraticBezierTo(
         (width / 2),
         (height / 2) - 0,
-        (width / 2) - 10,
-        height - 10,
+        (width / 2) - 10.w,
+        height - 10.h,
       )
-      ..arcToPoint(Offset((width / 2) - 30, height), radius: const Radius.circular(25))
+      ..arcToPoint(Offset((width / 2) - 30.w, height), radius: const Radius.circular(25).r)
 
       /// left box
 
-      ..lineTo(20, height)
-      ..arcToPoint(const Offset(20, 0), radius: const Radius.circular(25));
+      ..lineTo(20.w, height)
+      ..arcToPoint(Offset(20.w, 0), radius: const Radius.circular(25).r);
 
     /// draw fill
     canvas.drawPath(path, paint);
@@ -66,20 +67,20 @@ class PortfolioSwitchClip extends CustomPainter {
     currentIndex == 0
         ? canvas.drawPath(
             Path()
-              ..moveTo(20, 0)
-              ..lineTo((width / 2) - 30, 0)
-              ..arcToPoint(Offset((width / 2) - 30, height), radius: const Radius.circular(25))
-              ..lineTo(20, height)
-              ..arcToPoint(const Offset(20, 0), radius: const Radius.circular(25)),
+              ..moveTo(20.w, 0)
+              ..lineTo((width / 2) - 30.w, 0)
+              ..arcToPoint(Offset((width / 2) - 30.w, height), radius: const Radius.circular(25).r)
+              ..lineTo(20.w, height)
+              ..arcToPoint( Offset(20.w, 0), radius: const Radius.circular(25).r),
             paint..color = const Color(0xff83CCFB),
           )
         : canvas.drawPath(
             Path()
-              ..moveTo((width / 2) + 30, 0)
-              ..lineTo(width - 20, 0)
-              ..arcToPoint(Offset(width - 20, height), radius: const Radius.circular(25))
-              ..lineTo((width / 2) + 30, height)
-              ..arcToPoint(Offset((width / 2) + 30, 0), radius: const Radius.circular(25)),
+              ..moveTo((width / 2) + 30.w, 0)
+              ..lineTo(width - 20.w, 0)
+              ..arcToPoint(Offset(width - 20.w, height), radius: const Radius.circular(25).r)
+              ..lineTo((width / 2) + 30.w, height)
+              ..arcToPoint(Offset((width / 2) + 30.w, 0), radius: const Radius.circular(25).r),
             paint..color = const Color(0xff83CCFB),
           );
   }
