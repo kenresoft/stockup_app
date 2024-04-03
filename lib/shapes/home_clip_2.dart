c1625dedf73f70ffdb1b8f397d76d346df805061
kenresoft <amadikenneth8@gmail.com>
2024-04-03 16:55:54 +0100

    Update home_clip_2.dart

--- lib/shapes/home_clip_2.dart
+++ lib/shapes/home_clip_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeClip2 extends CustomPainter {
   const HomeClip2(this.color);
@@ -8,7 +9,7 @@ class HomeClip2 extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final double width = size.width;
-    final double height = size.height + 10;
+    final double height = size.height + 10.h;
 
     /// OUTLINE BORDER
     final outlinePaint = Paint()
@@ -16,10 +17,10 @@ class HomeClip2 extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeJoin = StrokeJoin.round
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 2;
+      ..strokeWidth = 2.w;
 
     var outlinePath = layerPath(width, height, outlinePaint);
-    outlinePath = outlinePath.shift(const Offset(0, 22));
+    outlinePath = outlinePath.shift(Offset(0, 22.h));
     canvas.drawPath(outlinePath, outlinePaint);
 
     /// FILL
@@ -28,32 +29,32 @@ class HomeClip2 extends CustomPainter {
       ..style = PaintingStyle.fill;
 
     var fillPath = layerPath(width, height, fillPaint);
-    fillPath = fillPath.shift(const Offset(0, 22));
+    fillPath = fillPath.shift(Offset(0, 22.h));
     canvas.drawPath(fillPath, fillPaint);
   }
 
   Path layerPath(double width, double height, Paint paint) {
     final path = Path()
       ..moveTo(0, 0)
-      ..arcToPoint(Offset(35, -15), radius: const Radius.circular(20), clockwise: true)
-      ..arcToPoint(Offset(70, 0), radius: const Radius.circular(45), clockwise: false)
+      ..arcToPoint(Offset(35.w, -15.h), radius: const Radius.circular(20).r, clockwise: true)
+      ..arcToPoint(Offset(70.w, 0), radius: const Radius.circular(45).r, clockwise: false)
 
       /// Top Right
-      ..lineTo(width - 20, 0)
-      ..arcToPoint(Offset(width, 20), radius: const Radius.circular(20), clockwise: true)
+      ..lineTo(width - 20.w, 0)
+      ..arcToPoint(Offset(width, 20.h), radius: const Radius.circular(20).r, clockwise: true)
 
       ///
-      ..lineTo(width, height - 20)
-      ..arcToPoint(Offset(width - 20, height), radius: const Radius.circular(20), clockwise: true)
+      ..lineTo(width, height - 20.h)
+      ..arcToPoint(Offset(width - 20.w, height), radius: const Radius.circular(20).r, clockwise: true)
 
       /// Bottom Right
-      ..lineTo(30, height)
-      ..arcToPoint(Offset(10, height - 20), radius: const Radius.circular(20), clockwise: true)
+      ..lineTo(30.w, height)
+      ..arcToPoint(Offset(10.w, height - 20.h), radius: const Radius.circular(20).r, clockwise: true)
 
       /// Bottom Left
       ..lineTo(10, 70)
-      ..arcToPoint(Offset(5, 55), radius: const Radius.circular(35), clockwise: false)
-      ..arcToPoint(Offset(0, 30), radius: const Radius.circular(45), clockwise: true)
+      ..arcToPoint(Offset(5.w, 55.h), radius: const Radius.circular(35).r, clockwise: false)
+      ..arcToPoint(Offset(0, 30.h), radius: const Radius.circular(45).r, clockwise: true)
 
       /// Top Left
       ..lineTo(0, 0);
